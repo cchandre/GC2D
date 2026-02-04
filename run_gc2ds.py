fa33662b3be5853b6855ac974ab52cdcdabf0568
Cristel Chandre <cchandre@gmail.com>
2026-02-04 17:58:41 +0100

    Update run_gc2ds.py

--- run_gc2ds.py
+++ run_gc2ds.py
@@ -18,9 +18,9 @@ n_data = 200
 n_process = 100
 
 default_time_step = 0.1
-solver = 'BM4'
+solver = 'RK45'
 default_omega = None
-projection = 'midpoint'
+projection = None
 
 parameters = {"A": A, "M": M}
 gc = GC2Ds(parameters)
@@ -46,8 +46,10 @@ elif mode == 'step':
 def run_one(param):
     step = param if mode == 'step' else default_time_step
     om = param if mode == 'omega' else default_omega 
-    sol = gc.integrate(z0, t_eval, timestep=step, omega=om, display=False, solver=solver, extension=True, check_energy=True, projection=projection, tol=1e-10, max_iter=100)
-    print(f"{mode} = {param:.3e} | error = {sol.err / Ntraj} | CPU_time = {int(sol.cpu_time)}s with projection = {sol.projection} and proj_dist = {sol.proj_dist}")
+    sol = gc.integrate(z0, t_eval, timestep=step, omega=om, display=False, solver=solver, extension=True, check_energy=True, projection=projection, tol=1e+10, max_iter=100)
+    if not hasattr(sol, 'projection'):
+        sol.projection = None
+        sol.proj_dist = None
     return {"A": A, "M": M, "Ntraj": Ntraj, "n_max": n_max, "solver": solver, "timestep": sol.step, "omega": om, 
         "error": sol.err / Ntraj, "cpu_time": sol.cpu_time, "projection": sol.projection, "proj_dist": sol.proj_dist}
 
